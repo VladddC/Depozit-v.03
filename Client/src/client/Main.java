af40d1c960fbc2d6c760da855abf17e71f113b2e
Vlad.C <Vlad.C@VCA-NB2.abs.ro>
2017-02-17 14:37:17 +0200

    Basic Client Main

--- Client/src/client/Main.java
+++ Client/src/client/Main.java
@@ -5,17 +5,44 @@
  */
 package client;
 
-/**
- *
- * @author Freeware Sys
- */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
+      
     public static void main(String[] args) {
-        // TODO code application logic here
+      try{
+          Socket socket = new Socket("localhost",4321);
+          
+          PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
+          BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+          
+          //citire din consola
+          BufferedReader consola = new BufferedReader(new InputStreamReader(System.in));
+          
+          while(true){
+              System.out.print("Client : ");
+              String line = consola.readLine();
+              
+              out.print(line);
+              
+              line = in.readLine();
+              System.out.print("Server : " + line);
+              
+              if (line.equalsIgnoreCase("end")){
+                  break;
+              }
+              
+          }
+          // stop citire consola 
+      }catch(Exception e)
+      {e.printStackTrace();} 
+        
+        
     }
     
 }
